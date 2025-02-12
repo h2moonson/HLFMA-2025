41bd15e4901ff181e1141529ac8629a61a0c4404
Heemoon Son <133964383+h2moonson@users.noreply.github.com>
2025-02-12 19:19:55 +0900

    Delete src/gps/build directory

--- src/gps/build/ublox_f9p/ublox_gps/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hmson/ieve_2025/src/gps/src/ublox_f9p/ublox_gps
-# Build directory: /home/hmson/ieve_2025/src/gps/build/ublox_f9p/ublox_gps
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
